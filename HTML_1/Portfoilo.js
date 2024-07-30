43a15760dbdee36a05126bb23798dfdc72642280
o.morris <loganllama15@gamil.com>
2024-07-30 14:10:23 -0400

    my greeting works. the next thing ineed to do is figure out how link my other html page as well as style those pages, also need to add more javascipt to my pages. i'm continue playing around with javascript

---
+++ HTML_1/Portfoilo.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    alert('Hello, Welcome to Ollies Archives');
+});
+
+// Get the elements from the DOM
+const greetingElement = document.getElementById('greeting');
+
